bb8e5f052757d11b7fc996b16611475ddc4680b9
Dongathan-Jong <duclingsyt@gmail.com>
2024-08-01 20:41:00 -0400

    Update spotify.h

--- spotify.h
+++ spotify.h
@@ -73,6 +73,70 @@ String loadRefreshToken() {
   return "";
 }
 
+int code = http.GET();
+    Serial.print("Response code: ");
+    Serial.println(code);
+    if(code == 301) {
+        // Redirect: save the cookies and send them back.
+        mxmCookie = http.header("Set-Cookie");
+        delay(100);
+        http.addHeader("Cookie", mxmCookie);
+        code = http.GET();
+        Serial.print("Response code: ");
+        Serial.println(code);
+    } else if(code != 200) {
+        return;
+    }
+
+    StaticJsonDocument<192> filter;
+
+    JsonObject lyric_filter = filter["message"]["body"]["macro_calls"]["track.subtitles.get"].createNestedObject("message");
+    lyric_filter["header"]["available"] = true;
+    lyric_filter["body"]["subtitle_list"][0]["subtitle"]["subtitle_body"] = true;
+
+    DeserializationError error = deserializeJson(lyricDoc, client, DeserializationOption::Filter(filter), DeserializationOption::NestingLimit(12));
+    if (error) {
+        Serial.print(F("deserializeJson() failed: "));
+        Serial.println(error.f_str());
+        return;
+    }
+
+    int lyric_available = lyricDoc["message"]["body"]["macro_calls"]["track.subtitles.get"]["message"]["header"]["available"];
+    if(lyric_available) {
+        p_lyric_start = lyricDoc["message"]["body"]["macro_calls"]["track.subtitles.get"]["message"]["body"]["subtitle_list"][0]["subtitle"]["subtitle_body"];
+        p_lyric_next = p_lyric_start;
+    }
+
+    http.end();
+}
+
+void saveRefreshToken(String refreshToken) {
+    File f = LittleFS.open(F("/sptoken.txt"), "w");
+    if (!f) {
+        Serial.println(F("Failed to write sptoken"));
+        return;
+    }
+    f.println(refreshToken);
+    f.close();
+    Serial.println(F("Saved token"));
+}
+
+String loadRefreshToken() {
+    File f = LittleFS.open(F("/sptoken.txt"), "r");
+    if (!f) {
+        Serial.println(F("Failed to read sptoken"));
+        return "";
+    }
+    while(f.available()) {
+        String token = f.readStringUntil('\r');
+        Serial.println(F("Loaded token"));
+        f.close();
+        return token;
+    }
+    return "";
+}
+
+
 unsigned int parseInt(const char* ptr) {
   unsigned int ret = 0;
   while(*ptr >= '0' && *ptr <= '9') {
